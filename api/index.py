f98d84df2d899d58f3ff18f0a7b2c25250f8a498
VS <wiliyamin101001@gmail.com>
2025-08-27 00:40:48 +0200

    Fix

--- api/index.py
+++ api/index.py
@@ -1,7 +1,5 @@
-import sys
 from pathlib import Path
+import sys
 ROOT = Path(__file__).resolve().parents[1]
-
-sys.path.append(str(ROOT))
-
+sys.path.insert(0, str(ROOT))
 from backend.app import app
